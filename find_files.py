40ad33d9bd11ad698b00e194a19182f5af2fd370
zhangxiaobo <zxb708@126.com>
2017-03-21 21:32:25 +0800

    8-10. 编程实例 - 查找文件 【问题描述】 用 Python 做运维或写自动化脚本时，难免要和文件打交道。 要求：输入关键字，列出指定文件夹中的所有文件名中含有此关键字的文件，以及文件内容中含有此关键字的文件。 主要涉及内容：文件路径、输入输出、字符串匹配

--- find_files.py
+++ find_files.py
@@ -7,44 +7,40 @@
 
 import os
 import re
-def find_files(find_path="D:\\2017\\crossin", core_key="test"):
+def find_files(find_path=r"D:\2017\crossin", core_key="test"):
     #定义的函数
     #参数为路径和关键字
-    #返回值为一个list和布尔值组成的tuple
+    #返回值为一个list
+    #如果list为空，则表示没有找到匹配的文件
+    #否则对应匹配的文件列表
     re_list = []
-    re_flag = False
     for root, dirs, files in os.walk(find_path):
         for name in files:
             pattern = core_key
             file_path = root + os.sep + name
-            if name == "find_files.py":
-                print("hello file:" + name)
-            if re.search(pattern, file_path):
-                re_list.append(root + os.sep + name)
-                re_flag = True
+            #if re.search(pattern, file_path):
+            if name.find(pattern) != -1:
+                re_list.append(file_path)
             else:
-                with open(file_path, "r") as f:
-                    if re.search("find_files.py", file_path):
-                        print("test")
+                with open(file_path, "r", encoding="utf-8") as f:
                     try:
-                        if re.search(pattern, f.read().encode("cp936")):
-                            re_list.append(file_path)
-                            re_flag = True
+                        #if re.search(pattern, f.readlines()):
+                        for l in f.readlines():
+                            if pattern in l:
+                                re_list.append(file_path)
+                                break
                     except:
                         pass
-
-    for file in re_list:
-        print(str(file))
     #返回值
-    return (re_list, re_flag)
+    return  re_list
 
 if __name__ == '__main__':
     dst = input("please enter the dir:")
     search_key = input("please enter the key word:")
 
-    saved_list, ret_flag = find_files(dst, search_key)
-    if ret_flag:
-        for file in saved_list:
-            print(file)
-    else:
-        print("No files found.")
+    saved_list = find_files(dst, search_key)
+    count = 0
+    for file in saved_list:
+        count += 1
+        print(file)
+    print("There are %d files matched." % count)
